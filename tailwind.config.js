7296159a37988af1e23d64bf0c75f365a0e55109
kwawbaidoo <kwaw.baidoo@gmail.com>
2024-05-29 04:13:37 +0000

    working hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,17 @@ module.exports = {
   content: ['./public/**/*.html'],
   theme: {
     extend: {
+      colors:{
+        'back': '#0F1624'
+      },
       backgroundImage:{
         'bg-gradient': 'linear-gradient(270deg, #13ADC7 0%, #6978D1 66.67%, #945DD6 100%)',
 
-      }
+      },
+
+      boxShadow:{
+        'neon-button': '0px 10px 30px 0px #00B2FF59'
+      },
     },
   },
   plugins: [],
